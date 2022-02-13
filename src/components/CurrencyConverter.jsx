d59196d05c6f135554ec960c1a039926dcb923ab
Kelvis Fernando <kelvisdelapena@gmail.com>
2022-02-13 19:29:03 -0300

    Created the inputs select, and made a array for store all the currencies avaliable, and into the options was made a map for show the currencies and set and show with useState

--- src/components/CurrencyConverter.jsx
+++ src/components/CurrencyConverter.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import ExchangeRate from "./ExchangeRate";
 
 const CurrencyConverter = () => {
+  const currencies = ['BTC', 'ETH', 'USD', 'XRP', 'LTC', 'ADA'];
+  const [chosenPrimaryCurrency, setChosenPrimaryCurrency] = useState('BTC');
+  const [chosenSecondaryCurrency, setChosenSecondaryCurrency] = useState('BTC');
+  console.log(chosenPrimaryCurrency);
+  console.log(chosenSecondaryCurrency);
+
   return (
     <div className="currency-converter">
       <h2>CurrencyConverter</h2>
@@ -9,11 +16,41 @@ const CurrencyConverter = () => {
           <tr>
             <td>Primary Currency</td>
             <td>
-                <input 
-                    type="number"
-                    name="currency-amount-1"
-                    value={""}
-                />
+              <input
+                type="number"
+                name="currency-amount-1"
+                value={""}
+              />
+            </td>
+            <td>
+              <select
+                name="current-option-1"
+                value={chosenPrimaryCurrency}
+                className="currency-options"
+                onChange={(e) => setChosenPrimaryCurrency(e.target.value)}
+              >
+                {currencies.map((currency, index) => (<option key={index}>{currency}</option>))}
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <td>Second Currency</td>
+            <td>
+              <input
+                type="number"
+                name="currency-amount-2"
+                value={""}
+              />
+            </td>
+            <td>
+              <select
+                name="current-option-2"
+                value={chosenSecondaryCurrency}
+                className="currency-options"
+                onChange={(e) => setChosenSecondaryCurrency(e.target.value)}
+              >
+                {currencies.map((currency, index) => (<option key={index}>{currency}</option>))}
+              </select>
             </td>
           </tr>
         </tbody>
