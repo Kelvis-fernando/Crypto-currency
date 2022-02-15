5e54f4ebe9d9684640c5954d7245b932f3b715e5
Kelvis Fernando <kelvisdelapena@gmail.com>
2022-02-15 19:00:40 -0300

    Made on component news feed the map for show all the news for users, and limited the response api for only 15 news

--- src/components/NewsFeed.jsx
+++ src/components/NewsFeed.jsx
@@ -1,21 +1,44 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const NewsFeed = () => {
+  const [articles, setArticles] = useState(null);
+
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      url: "https://crypto-news-live3.p.rapidapi.com/news",
+      headers: {
+        "x-rapidapi-host": "crypto-news-live3.p.rapidapi.com",
+        "x-rapidapi-key": "ad1a634496mshcc6365583085806p15f9bdjsn07f41c943008",
+      },
+    };
+
+    axios
+      .request(options)
+      .then((response) => {
+        setArticles(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  console.log(articles);
+
+  const first15Articles = articles?.splice(0, 7);
+
   return (
     <div className="news-feed">
       <h2>NewsFeed</h2>
       <div>
-        <h3>Title</h3>
-        <p>Message</p>
-        <a href="www.google.com">Link</a>
-      </div>
-      <div>
-        <h3>Title</h3>
-        <p>Message</p>
-        <a href="www.google.com">Link</a>
-      </div>
-      <div>
-        <h3>Title</h3>
-        <p>Message</p>
-        <a href="www.google.com">Link</a>
+        {first15Articles.map((article) => (
+          <div>
+            <h3>{article.title}</h3>
+            <p>{article.source}</p>
+            
+          </div>
+        ))}
       </div>
     </div>
   );
